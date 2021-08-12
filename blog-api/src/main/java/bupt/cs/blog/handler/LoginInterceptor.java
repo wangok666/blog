5e9b2d8e01a6545bda8fb426a757db2ed8d6a60e
wangok666 <785404277@qq.com>
2021-08-12 19:50:32 +0800

    登录拦截器

---
+++ blog-api/src/main/java/bupt/cs/blog/handler/LoginInterceptor.java
@@ -0,0 +1,60 @@
+package bupt.cs.blog.handler;
+
+import bupt.cs.blog.dao.pojo.SysUser;
+import bupt.cs.blog.service.LoginService;
+import bupt.cs.blog.vo.ErrorCode;
+import bupt.cs.blog.vo.Result;
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private LoginService loginService;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        /**
+         * 1. 需要判断 请求的接口路径是否为HandlerMethod(controller方法)
+         * 2. 判断token是否为空 如果为空 则未登录
+         * 3. 如果token不为空，登录验证loginService checkToken
+         * 4. 如果认证成功，方形即可
+         * * */
+        if (!(handler instanceof HandlerMethod)) {
+            return true;
+        }
+        String token = request.getHeader("Authorization");
+        log.info("=================request start===========================");
+        String requestURI = request.getRequestURI();
+        log.info("request uri:{}",requestURI);
+        log.info("request method:{}",request.getMethod());
+        log.info("token:{}", token);
+        log.info("=================request end===========================");
+
+        if (token == null){
+            Result result = Result.fail(ErrorCode.NO_LOGIN.getCode(), "未登录");
+            response.setContentType("application/json;charset=utf-8");
+            response.getWriter().print(JSON.toJSONString(result));
+            return false;
+        }
+
+        SysUser sysUser = loginService.checkToken(token);
+        if (sysUser == null) {
+            Result result = Result.fail(ErrorCode.NO_LOGIN.getCode(), "未登录");
+            response.setContentType("application/json;charset=utf-8");
+            response.getWriter().print(JSON.toJSONString(result));
+            return false;
+        }
+
+        return true;
+    }
+}
